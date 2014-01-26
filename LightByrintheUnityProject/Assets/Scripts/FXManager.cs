80eb4b4e0e5109a0ca39a964a9a3baea2743f59f
podpod <andy.duboc@gmail.com>
2014-01-26 14:03:25 +0100

    updated levels

---
+++ LightByrintheUnityProject/Assets/Scripts/FXManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FXManager : MonoBehaviour {
+
+	public List<GameObject> FXs;
+
+	private static FXManager s_Instance = null;
+	public static FXManager instance
+	{
+		get
+		{
+			return s_Instance;
+		}
+	}
+
+	void Awake()
+	{
+		s_Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public GameObject Get(string name)
+	{
+		for(int i = 0; i < FXs.Count; ++i)
+		{
+			if(FXs[i].name == name)
+			{
+				return FXs[i];
+			}
+		}
+
+		return null;
+	}
+}
