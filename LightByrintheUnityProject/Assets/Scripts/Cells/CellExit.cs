3a982cb22986d3b0d6ea0e7cb9cc20da3c0dec65
podpod <andy.duboc@gmail.com>
2014-01-26 14:11:18 +0100

    build settings

--- LightByrintheUnityProject/Assets/Scripts/Cells/CellExit.cs
+++ LightByrintheUnityProject/Assets/Scripts/Cells/CellExit.cs
@@ -3,6 +3,26 @@ using System.Collections;
 
 public class CellExit : Cell 
 {
+	private GameObject _overlay;
+
+	private float intervaleSin = 1.0f;
+	private float intervaleCur = 0.0f;
+
+	public override void Start()
+	{
+		base.Start ();
+
+		//_overlay = (GameObject)GameObject.Instantiate(FXManager.instance.Get("Fxs_Finish_A"));
+		//_overlay.transform.parent = transform;
+		//_overlay.transform.position = transform.position; //new Vector3(transform.position.x, transform.position.y, -0.2f);
+		//_overlay.transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
+	}
+
+	public override void Update()
+	{
+		//_overlay.renderer.material.color = new Color(1.0f, 1.0f, 1.0f, Mathf.Sin(Time.time)+1.0f);
+	}
+
 	public override void OnActorEnter(Actor a)
 	{
 		a.OnExit();
