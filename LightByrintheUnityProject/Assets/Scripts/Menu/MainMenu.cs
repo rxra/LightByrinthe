c79df44c5fb9f92e8470bf168c240081ef1733bb
podpod <andy.duboc@gmail.com>
2014-01-26 08:12:26 +0100

    added press touch

--- LightByrintheUnityProject/Assets/Scripts/Menu/MainMenu.cs
+++ LightByrintheUnityProject/Assets/Scripts/Menu/MainMenu.cs
@@ -2,9 +2,15 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
-		Application.LoadLevel("TestMap");
+	}
+
+	void Update()
+	{
+		if(Input.anyKeyDown) {
+			Application.LoadLevel("TestMap");
+		}
 	}
 }
